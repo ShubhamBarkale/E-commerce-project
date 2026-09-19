f04ee1f1c2fa4ee63580601c52c47e980bc3bb12
ShubhamBarkale <shubhambarkale674@gmail.com>
2026-09-19 10:57:44 +0530

    Connect React with Django Product API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,43 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [product, setProduct] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/")
+    fetch("http://127.0.0.1:8000/api/product/")
       .then((response) => response.json())
-      .then((data) => setMessage(data.message))
+      .then((data) => setProduct(data))
       .catch((error) =>
-        console.error("Error fetching message:", error)
+        console.error("Error fetching products:", error)
       );
   }, []);
 
   return (
-    <div>
-      <h1>Message from Backend</h1>
-      <p>{message || "Loading...."}</p>
+    <div className="min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold underline">
+        <div className="container mx-auto p-4">
+          {product.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white p-4 rounded shadow mb-4"
+            >
+              <h2 className="text-xl font-semibold">
+                {product.name}
+              </h2>
+
+              <p className="text-gray-600">
+                {product.description}
+              </p>
+
+              <p className="text-gray-800 font-bold">
+                ${product.price}
+              </p>
+            </div>
+          ))}
+        </div>
+      </h1>
     </div>
   );
 }
 
 export default App;
-
